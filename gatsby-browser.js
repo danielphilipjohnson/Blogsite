ba7248f8c1fa6c9f3036dc9e5aae779a80c212a4
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-04-28 18:26:48 +0100

    enhancement: added googlefont plugin to use custom font for blog content

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,2 @@
 import "./src/styles/global.css";
-// require("prismjs/themes/prism-coy.css");
+import "@fontsource/quattrocento-sans"; // Defaults to weight 400.
--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         path: `${__dirname}/content/`,
       },
     },
+
     {
       resolve: `gatsby-transformer-remark`,
       options: {
