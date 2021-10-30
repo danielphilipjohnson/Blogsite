d8a2c915b67938efdc1fd06917e15d7634e144c5
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-10-30 19:02:16 +0100

    Finished archive page

--- src/pages/archives.js
+++ src/pages/archives.js
@@ -5,13 +5,15 @@ import Layout from "../components/layout";
 import ArchiveBlogs from "../components/archive-blogs";
 
 export default function Home({ data }) {
+  const articles = data.allStrapiArticle.nodes;
+
   return (
     <>
       <Layout seo={data.strapiArchivepage.seo}>
         <div className="pt-2 md:pt-8 container pb-12">
           <ArchiveBlogs
-            blogs={data.blogs.edges}
-            count={data.blogs.totalCount}
+            blogs={articles}
+            count={data.allStrapiArticle.totalCount}
             title={"Blogs from Daniel — Archive"}
           />
         </div>
@@ -22,37 +24,32 @@ export default function Home({ data }) {
 
 export const query = graphql`
   query {
-    blogs: allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      totalCount
-      edges {
-        node {
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            category
-            cover {
-              childImageSharp {
-                gatsbyImageData(
-                  width: 800
-                  height: 500
-                  formats: [AUTO, WEBP, AVIF, PNG]
-                )
-              }
+
+    allStrapiArticle {
+      nodes {
+        id
+        published_at
+        title
+        slug
+        author {
+          name
+        }
+        
+        category {
+          name
+        }
+        description
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
             }
           }
-          excerpt
         }
       }
+      totalCount
     }
-    categories: allMarkdownRemark {
-      distinct(field: frontmatter___category)
-    }
+  
     strapiArchivepage {
       seo {
         metaTitle
