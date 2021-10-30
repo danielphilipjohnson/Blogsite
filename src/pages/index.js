004d139d149501b8b74b6cc696557012c4f4eda3
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-10-30 21:39:49 +0100

    Added subscribe block to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout";
 import Categories from "../components/categories";
 import Banner from "../components/banner";
 import RecentBlogs from "../components/blogs/recent";
+import LetsConnect from "../components/hero/lets-connect";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <Banner />
         <RecentBlogs title={"Latest Posts"} />
         <Categories />
+        <LetsConnect/>
       </Layout>
     </>
   );
